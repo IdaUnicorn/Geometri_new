3dbfed2fabd6685bbb183a7173e34532d1fe5dbb
Ida S <idax362k@zbc.dk>
2020-09-07 09:51:14 +0200

    Add project files.

---
+++ Geometri_new/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_new
+{
+    class Program
+    {
+        public static float side = 0;
+        public static string color;
+        static void Main(string[] args)
+        {
+            //Nu kan du teste dit objekt. Opret et Square objekt i din main metode – du bestemmer selv hvor ”stor” a skal være. Herefter, 
+            //implementer en metode der ligner den du lavede i trin 4, men denne metode skal beregne arealet på dit kvadrat. Herefter lav flere objekter
+            Console.WriteLine("What is the measure of the side ? ");
+            Console.WriteLine("what is the color of the square ?");
+            try
+            {
+                side = float.Parse(Console.ReadLine()); //convert string to float.
+                color = Console.ReadLine(); 
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("You did not type a number");
+            }
+            CalcSide();
+            CalcArea();
+            Console.WriteLine($"The perimeter er {CalcSide()}"); //conventerer selv fra string fil tal 
+            Console.WriteLine($"The area is {CalcArea()}"); //conventerer selv fra string fil tal 
+            Console.WriteLine($"The cubic meter is {CalcCubic()}");  //conventerer selv fra string fil tal
+            Console.WriteLine($"The color is {Color()}"); //laver en mere property.
+            Console.ReadKey();
+        }
+        //laver en metode
+        //Prøv at kalde metoden som du oprettede i trin 4 og udskriv resultatet til konsollen.
+        public static float CalcSide() 
+        {
+            return side * 4;
+        }
+        
+        public static float CalcArea()
+        {
+            return side * side;
+        }
+
+        public static float CalcCubic()
+        {
+            return side * 3;
+        }
+
+        public static string Color()
+        {
+            return color;
+        }
+    }
+}
---
+++ Geometri_new/Square.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_new
+{
+    //Opret en ny klasse i dit projekt, ved at højre-klikke på dit projekt i ”Solution Explorer”, 
+    //kald klassen Square. Tjek at din klasse er sat til public.
+    public class Square
+    {
+        private float side;
+        //Inde i klassen oprettes derfor kun et felt (det skal være private), du kan kalde attributten 
+        //side eller a, du vælger selv datatypen (Tip: Det kan ikke være en string)
+        public float Side //nu er jeg i proberties, properties skrives altid med stort
+        {
+            get { return this.side; }
+            set { this.side = value; }
+        }
+        //Opret en konstruktør, som tager side med som parameter. Inde i konstruktøren kan du ved brug af this, 
+        //initialisere din attribut
+        public Square(float side)
+        {
+            this.side = side;
+        }
+        //Opret en tom konstruktør – default konstruktør. Der skal ikke være nogen parametre
+        private Square() //man må have så mange konstruktører man vil, så længe at de har forskellige variabler. 
+        {}
+    }
+
+    public class Make_Constructor //starter med at lave en ny klasse 
+    {
+        public Make_Constructor ();
+
+    }
+
+}
+
